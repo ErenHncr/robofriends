5c096bf2cd1836a938394e4d266af5936f101d06
erenhncr <erenhncr@gmail.com>
2020-04-29 10:31:53 +0300

    set up redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundry from '../components/ErrorBoundry';
-import './App.css';
 import Scroll from '../components/Scroll';
+import './App.css';
+
+import { setSearchField } from '../actions';
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
+
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
-      searchfield: ''
+      robots: []
     }
   }
 
@@ -23,25 +39,25 @@ class App extends Component {
 
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  }
+  // onSearchChange = (event) => {
+  //   this.setState({ searchfield: event.target.value });
+  // }
 
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes
-        (searchfield.toLowerCase())
+        (searchField.toLowerCase())
     })
 
     if (!robots.length) {
-      //this.setState({ robots: robotsArray });
       return <h1>Loading...</h1>
     }
     return (
       <div className='tc' >
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundry>
             <CardList robots={filteredRobots} />
@@ -54,4 +70,4 @@ class App extends Component {
 
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
