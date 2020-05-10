731bdc24ba9bf7323ce89bfb38271529da7534ae
erenhncr <erenhncr@gmail.com>
2020-05-10 19:44:14 +0300

    imgs added

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,15 +3,13 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <label style={{ color: "white", marginRight: '10px' }}>
-        Enter:
-        <input style={{ marginRight: '10px' }}
-          className='pa3 ba b--green bg-lightest-blue'
-          type='search'
-          placeholder='search robots'
-          onChange={searchChange}
-        />
-      </label>
+      <input
+        aria-label='Search Robots'
+        className='pa3 ba b--green bg-lightest-blue'
+        type='search'
+        placeholder='search robots'
+        onChange={searchChange}
+      />
     </div>
   );
 };
