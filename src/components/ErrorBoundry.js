5099bc12e9f29355d4b1438c768449f42c94803d
erenhncr <erenhncr@gmail.com>
2020-05-16 14:55:44 +0300

    added test for error

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -9,7 +9,7 @@ class ErrorBoundry extends Component {
   }
 
   componentDidCatch(error, info) {
-    this.setState({ hasError: error })
+    this.setState({ hasError: true })
   }
   render() {
     if (this.state.hasError) {
